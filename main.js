f04f77a9c43c8669bffb53a89a4a68b582dcb316
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-24 14:49:15 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+var c=document.getElementById('myCanvas');
+var ctx=c.getContext('2d');
+function draw(){
+ctx.fillStyle("#000000");
+ctx.fillRect(0,0,c.width,c.height);
+ctx.fillStyle("#FFFFFF");
+for(var i=0;i<c.height;i+=2){
+for(var ii=0;ii<c.width;ii+=2){
+if(Math.random()<.25)
+ctx.fillRect(i,ii,2,2);
+}
+}
